7ae5bbed7cc7d7c26f73845bc04935681639e524
g.fantini <gabriele.fantini@salesagility.com>
2020-07-17 21:31:07 +0200

    Project & App & Main modules Creation

--- billbuddy/remindo/admin.py
+++ billbuddy/remindo/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Visits, Reminders
 
-# Register your models here.
+admin.site.register(Visits)
+admin.site.register(Reminders)
--- billbuddy/remindo/models.py
+++ billbuddy/remindo/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Reminders(models.Model):
+    title = models.CharField(max_length=150)
+    message = models.TextField()
+    delivery_time = models.DateTimeField()
+
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reminder_receiver')
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reminder_sender')
+    
+    def __srt__(self):
+        return self.title + " " + self.delivery_time
+    
+class Visits(models.Model):
+    time = models.DateTimeField()
+    ip_address = models.CharField(max_length=32)
+    visitor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+    def __srt__(self):
+        return self.visitor + " " + self.time 
+    
